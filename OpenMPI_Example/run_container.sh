5d10731ff41b3fb5a4559347a3813e1e2e4f8633
Ezequiel Torres <efeyuk@medallia.com>
2018-03-14 22:48:20 -0300

    Remove image versioning from OpenMPI Example

--- OpenMPI_Example/run_container.sh
+++ OpenMPI_Example/run_container.sh
@@ -3,19 +3,18 @@ export ROOTPATH="$(dirname "$(readlink -f "$0")")"
 
 usage() {
     echo "Usage:"
-    echo -e "\t./run_container.sh [CONTAINER_EXAMPLE_VERSION] [MPI_PROCESSORS_NUMBER]"
+    echo -e "\t./run_container.sh [MPI_PROCESSORS_NUMBER]"
     exit 1
 }
 
 # Script receives two parameters
-# 1: Version of the example to run
-# 2: Processor to run in the MPI simulation
-if [ "$#" -ne "2" ] ; then
+# 1: Amount of Processors to run in the MPI simulation
+if [ "$#" -ne "1" ] ; then
     usage
 fi
 
 # Build the example
-sudo docker build -f $(pwd)/Dockerfile -t openmpi_example:$1 .
+sudo docker build -f $(pwd)/Dockerfile -t openmpi_example:v0.0.1 .
 
 # Run the example
-sudo docker run --rm -e PROCESSORS="$2" -it openmpi_example:$1 
+sudo docker run --rm -e PROCESSORS="$1" -it openmpi_example:v0.0.1 
